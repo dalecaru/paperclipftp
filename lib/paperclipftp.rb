8a8ca684f955ab262767b82fad9af0458b982e5b
Olek Poplavsky <olek@woodenbits.com>
2011-04-19 18:08:46 -0400

    fixed issues with tabs, trailing whitespaces and line endings

--- lib/paperclipftp.rb
+++ lib/paperclipftp.rb
@@ -2,37 +2,37 @@ module Paperclip
   module Storage
     module Ftp
       def self.extended base
-      	require 'net/ftp'
-      	base.instance_eval do
-      	  @ftp_credentials = parse_credentials
-  	    end
+        require 'net/ftp'
+        base.instance_eval do
+          @ftp_credentials = parse_credentials
+        end
       end
-      
+
       def ftp
-      	if @ftp.nil? || @ftp.closed?
-      	  @ftp = Net::FTP.new(@ftp_credentials[:host], @ftp_credentials[:username], @ftp_credentials[:password])
-  	    end
-  	    @ftp
+        if @ftp.nil? || @ftp.closed?
+          @ftp = Net::FTP.new(@ftp_credentials[:host], @ftp_credentials[:username], @ftp_credentials[:password])
+        end
+        @ftp
       end
-      
+
       def exists?(style = default_style)
-      	move_to_remote_path(File.dirname(path(style)))
-      	ftp.size(File.basename(path(style))) > 0 ? true : false
+        move_to_remote_path(File.dirname(path(style)))
+        ftp.size(File.basename(path(style))) > 0 ? true : false
       rescue Net::FTPPermError => e
-      	#File not exists
-      	false
+        #File not exists
+        false
       rescue Net::FTPReplyError => e
-      	ftp.close
-      	raise e
+        ftp.close
+        raise e
       end
 
       def to_file style = default_style
-      	@queued_for_write[style] || "ftp://#{path(style)}"
+        @queued_for_write[style] || "ftp://#{path(style)}"
       end
       alias_method :to_io, :to_file
 
       def flush_writes
-      	@queued_for_write.each do |style, file|
+        @queued_for_write.each do |style, file|
           file.close
           move_to_remote_path(File.dirname(path(style)))
           log("uploading #{path(style)}")
@@ -40,15 +40,15 @@ module Paperclip
         end
         @queued_for_write = {}
       rescue Net::FTPReplyError => e
-      	raise e
+        raise e
       rescue Net::FTPPermError => e
-      	raise e
+        raise e
       ensure
-      	ftp.close
+        ftp.close
       end
 
       def flush_deletes
-      	@queued_for_delete.each do |path|
+        @queued_for_delete.each do |path|
           begin
             move_to_remote_path(File.dirname(path))
             log("deleting #{path}")
@@ -58,34 +58,34 @@ module Paperclip
         end
         @queued_for_delete = []
       rescue Net::FTPReplyError => e
-      	raise e
+        raise e
       rescue Net::FTPPermError => e
-      	raise e
+        raise e
       ensure
-      	ftp.close 
+        ftp.close
       end
-      
+
       def move_to_remote_path(rpath)
-      	ftp.chdir("/")
-      	rpath.split(File::SEPARATOR).each do |rdir|
-      	  rdir = rdir.strip
-      	  unless rdir.blank?
-      	  	list = ftp.ls.collect { |f| f.split.last }
-      	    unless list.include?(rdir)
-      	     ftp.mkdir(rdir)
-  	  	    end
-  	  	    ftp.chdir(rdir)
-  	  	  end
- 	    end
+        ftp.chdir("/")
+        rpath.split(File::SEPARATOR).each do |rdir|
+          rdir = rdir.strip
+          unless rdir.blank?
+            list = ftp.ls.collect { |f| f.split.last }
+            unless list.include?(rdir)
+             ftp.mkdir(rdir)
+            end
+            ftp.chdir(rdir)
+          end
+        end
       end
-      
+
       def parse_credentials
-      	creds = YAML.load_file(File.join(RAILS_ROOT,"config","paperclipftp.yml"))
-      	creds = creds.stringify_keys
+        creds = YAML.load_file(File.join(RAILS_ROOT,"config","paperclipftp.yml"))
+        creds = creds.stringify_keys
         (creds[RAILS_ENV] || creds).symbolize_keys
       end
       private :parse_credentials
-      
-	end
+
+    end
   end
 end
